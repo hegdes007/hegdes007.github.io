ecf3cd076171f239db4e90a5877d73d0c1d67d26
Darshan M Hegde <68365486+hegdes007@users.noreply.github.com>
2021-04-02 12:39:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,36 +6,53 @@ const darshan = document.querySelector('.darshan img');
 const social = document.querySelector('.social');
 
 container3d.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
-    card.style.transform = `rotateY(${-xAxis}deg) rotateX(${yAxis}deg)`;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+  card.style.transform = `rotateY(${-xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 // ANIMATE IN
 container3d.addEventListener("mouseenter", (e) => {
-    info.style.transform = 'translateZ(75px)';
-    darshan.style.transform = 'translateZ(90px) rotate(-4deg)';
-    social.style.transform = 'translateZ(100px)';
-    card.style.transition = 'none';
+  info.style.transform = 'translateZ(75px)';
+  darshan.style.transform = 'translateZ(90px) rotate(-4deg)';
+  social.style.transform = 'translateZ(100px)';
+  card.style.transition = 'none';
 });
 
 // ANIMATE OUT
 container3d.addEventListener("mouseleave", (e) => {
-    info.style.transform = 'translateZ(0px)';
-    darshan.style.transform = 'translateZ(0px)';
-    social.style.transform = 'translateZ(0px)';
-    card.style.transition = 'all 0.5s ease';
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  info.style.transform = 'translateZ(0px)';
+  darshan.style.transform = 'translateZ(0px)';
+  social.style.transform = 'translateZ(0px)';
+  card.style.transition = 'all 0.5s ease';
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
 
-// DARK-MDE
-// document.querySelector('.dark-mode').addEventListener
-// ('click', () => {
-//     document.body.classList.toggle('dark')
-// })
 
+// TYPEWRITING EFFECT
+const texts = ['Engineer', 'Web Developer', 'Cricket Lover'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+  document.querySelector('.typing').textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 500)
+})();
+
+// DARK MODE
 // check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem('darkMode'); 
+let darkMode = localStorage.getItem('darkMode');
 
 const darkModeToggle = document.querySelector('.dark-mode');
 
@@ -45,14 +62,13 @@ const enableDarkMode = () => {
   // 2. Update darkMode in localStorage
   localStorage.setItem('darkMode', 'enabled');
 }
-
 const disableDarkMode = () => {
   // 1. Remove the class from the body
   document.body.classList.remove('dark');
   // 2. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
 }
- 
+
 // If the user already visited and enabled darkMode
 // start things off with it on
 if (darkMode === 'enabled') {
@@ -62,13 +78,13 @@ if (darkMode === 'enabled') {
 // When someone clicks the button
 darkModeToggle.addEventListener('click', () => {
   // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode'); 
-  
+  darkMode = localStorage.getItem('darkMode');
+
   // if it not current enabled, enable it
   if (darkMode !== 'enabled') {
     enableDarkMode();
-  // if it has been enabled, turn it off  
-  } else {  
-    disableDarkMode(); 
+    // if it has been enabled, turn it off  
+  } else {
+    disableDarkMode();
   }
 });
